d8da338553d602db76b9d68af86a8b1117180bea
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-17 20:43:43 +0800

    feat: migrate Supplier and Product in model

--- accounts/models.py
+++ accounts/models.py
@@ -37,3 +37,19 @@ class Employee(models.Model):
     def __str__(self):
         return self.name
 
+class Supplier(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    contact_person = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=100, null=True)
+    base_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    barcode = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return f"{self.product_name} | {self.barcode}"
