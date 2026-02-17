616de2f894e60d8fa19f8178bc40db6b6750b6ad
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-17 20:54:30 +0800

    feat: migrate branch, order and order item in model

--- accounts/models.py
+++ accounts/models.py
@@ -53,3 +53,47 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.product_name} | {self.barcode}"
+
+class BranchInventory(models.Model):
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.branch} | {self.product} | {self.quantity}"
+
+class Order(models.Model):
+    ORDER_STATUS = (
+    ('Pending', 'Pending'),
+    ('Completed', 'Completed'),
+    ('Cancelled', 'Cancelled'),
+    )
+
+    PAYMENT_METHOD = (
+    ('CASH', 'CASH'),
+    ('INSTALLMENT', 'INSTALLMENT'),
+    )
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+
+    order_date = models.DateField(auto_now_add=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    order_status = models.CharField(choices=ORDER_STATUS, max_length=100, null=True)
+    payment_method = models.CharField(choices=PAYMENT_METHOD, max_length=100, null=True)
+
+    def __str__(self):
+        return f"{self.id} | {self.branch.name}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+
+    def __str__(self):
+        return f"{self.quantity} | {self.product.name}"
+
+
